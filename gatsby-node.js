23a2694c590535ba866702d6d6d742ff4d782519
Ryan Warner <warner.ryan@gmail.com>
2020-06-12 19:02:31 -0400

    Create pages from Prismic posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,39 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // Query all Pages with their IDs and template data.
+  const pages = await graphql(`
+    {
+      allPrismicPost {
+        edges {
+          node {
+            data {
+              title {
+                text
+              }
+            }
+            id
+            first_publication_date
+            uid
+          }
+        }
+      }
+    }
+  `)
+
+  const postTemplate = path.resolve('./src/components/PostTemplate/PostTemplate.js')
+
+  // Create pages for each Post in Prismic.
+  pages.data.allPrismicPost.edges.forEach((edge) => {
+    console.log('edge.node.uid', edge.node.uid)
+    createPage({
+      path: `${edge.node.uid}`,
+      component: postTemplate,
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
